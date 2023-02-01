fa01e868e060543c2226492348e3279c20ecbd2b
kumarpal <kumarpal.nagar@mindinventory.com>
2023-02-01 18:53:26 +0530

    add micro-util library to project.

---
+++ micro_util/README.md
@@ -0,0 +1,3 @@
+# micro-util
+
+This is Micro-service library for "MicroserviceArchitectureDemo"(https://github.com/kp-nagar/MicroserviceArchitectureDemo) project. 
---
+++ micro_util/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+
+setuptools.setup(
+    name="micro_util",
+    version="0.1",
+    install_requires=[
+
+    ],
+    author="Kumarpal Nagar",
+    author_email="kumarpal.nagar@gmail.com",
+    description="Micro-service library which contains Common functionality.",
+    long_description="This is library which help to connect micro-services.",
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
