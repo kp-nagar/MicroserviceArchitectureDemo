48d9581a12b70f9cad77a941a01c4e2068455ab3
kumarpal <kumarpal.nagar@mindinventory.com>
2023-02-03 15:09:30 +0530

    Add unit test and change main app path.

--- Account/run.py
+++ Account/run.py
@@ -2,4 +2,4 @@ import uvicorn
 
 
 if __name__ == "__main__":
-    uvicorn.run("src.app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
---
+++ Account/tests/test_base_urls.py
@@ -0,0 +1,10 @@
+from app import app
+from fastapi.testclient import TestClient
+
+
+client = TestClient(app)
+
+def test_index_url():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == "account service is up."
